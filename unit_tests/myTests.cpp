aef1031f21ee04e15b53dc4d6c1f9d67273065b1
Dellvin <dellvin.black@gmail.com>
2020-03-05 00:51:43 +0300

    fix tests

--- unit_tests/myTests.cpp
+++ unit_tests/myTests.cpp
@@ -11,7 +11,7 @@ TEST(testMainFunc, test1) {
     ASSERT_EQ(true, true);
     long long decNumber = 123456;
     size_t sizeRomeStr = 0;
-    char *romeStr =  (char*)malloc(sizeRomeStr + 1);
+    char *romeStr;
     getRomeNumber(decNumber, romeStr, &sizeRomeStr);
     ASSERT_EQ(sizeRomeStr, 11);
     ASSERT_EQ(strcmp(romeStr, "CXXIIICDLVI"), 0);
