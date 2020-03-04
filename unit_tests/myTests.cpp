942b10c80e7e0cae03de890d10cd19dd3337694d
Dellvin <dellvin.black@gmail.com>
2020-03-05 01:27:22 +0300

    fff

--- unit_tests/myTests.cpp
+++ unit_tests/myTests.cpp
@@ -16,24 +16,24 @@ TEST(testMainFunc, test1) {
     ASSERT_EQ(sizeRomeStr, 11);
     ASSERT_EQ(strcmp(romeStr, "CXXIIICDLVI"), 0);
 
-//    long long num = 987;
-//    sizeRomeStr = 0;
-//    char *Str = (char*)malloc(sizeRomeStr + 1);
-//    Str=getRomeNumber(num, Str, &sizeRomeStr);
-//    ASSERT_EQ(sizeRomeStr, 9);
-//    ASSERT_EQ(strcmp(Str, "CMLXXXVII"), 0);
-//
-//    num = -234;
-//    sizeRomeStr = 0;
-//    char *irrcorrectStr = (char*)malloc(sizeRomeStr + 1);
-//    irrcorrectStr=getRomeNumber(num, irrcorrectStr, &sizeRomeStr);
-//    ASSERT_EQ(sizeRomeStr, 0);
+    long long num = 987;
+    sizeRomeStr = 0;
+    char *Str = (char*)malloc(sizeRomeStr + 1);
+    Str=getRomeNumber(num, Str, &sizeRomeStr);
+    ASSERT_EQ(sizeRomeStr, 9);
+    ASSERT_EQ(strcmp(Str, "CMLXXXVII"), 0);
+
+    num = -234;
+    sizeRomeStr = 0;
+    char *irrcorrectStr = (char*)malloc(sizeRomeStr + 1);
+    irrcorrectStr=getRomeNumber(num, irrcorrectStr, &sizeRomeStr);
+    ASSERT_EQ(sizeRomeStr, 0);
 //    ASSERT_EQ(irrcorrectStr, NULL);
 //
-//    num = 0;
-//    sizeRomeStr = 0;
-//    char *irrcorrectStrTwo = (char*)malloc(sizeRomeStr + 1);
-//    irrcorrectStrTwo=getRomeNumber(num, irrcorrectStrTwo, &sizeRomeStr);
-//    ASSERT_EQ(sizeRomeStr, 0);
+    num = 0;
+    sizeRomeStr = 0;
+    char *irrcorrectStrTwo = (char*)malloc(sizeRomeStr + 1);
+    irrcorrectStrTwo=getRomeNumber(num, irrcorrectStrTwo, &sizeRomeStr);
+    ASSERT_EQ(sizeRomeStr, 0);
 //    ASSERT_EQ(irrcorrectStrTwo, NULL);
 }
