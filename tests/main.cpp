1630125105331c56b94e0bbd3b8c4efc9b6cea8c
Dellvin <dellvin.black@gmail.com>
2020-03-08 01:11:31 +0300

    fix tests 4

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,11 +3,36 @@
 //
 
 #include <gtest/gtest.h>
-#include "../project/include/arabiantorome.h"
+
+extern "C" {
+#include <sys/stat.h>
+
+#include "arabiantorome.h"
+}
 
 
 TEST(testMainFunc, test1) {
-ASSERT_EQ(true, true);
+    ASSERT_EQ(true, true);
+    int64_t decNumber = 123456;
+    size_t sizeRomeStr = 0;
+    char *romeStr = NULL;
+    getRo
+    romeStr=getRomeNumber(decNumber, romeStr, &sizeRomeStr);
+    ASSERT_EQ(sizeRomeStr, 11);
+    ASSERT_EQ(strcmp(romeStr, "CXXIIICDLVI"), 0);
+
+    long long num = 987;
+    sizeRomeStr = 0;
+    char *Str = (char*)malloc(sizeRomeStr + 1);
+    Str=getRomeNumber(num, Str, &sizeRomeStr);
+    ASSERT_EQ(sizeRomeStr, 9);
+    ASSERT_EQ(strcmp(Str, "CMLXXXVII"), 0);
+
+    num=0;
+    ASSERT_EQ(checkNumber(num), -1);
+
+    num=-25536;
+    ASSERT_EQ(checkNumber(num), -1);
 }
 
 
