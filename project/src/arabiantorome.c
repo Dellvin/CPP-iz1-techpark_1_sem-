a73f3b10d03eacabee72cc80955df9892f77ff1e
Dellvin <dellvin.black@gmail.com>
2020-03-08 16:44:36 +0300

    add 1 test

--- project/src/arabiantorome.c
+++ project/src/arabiantorome.c
@@ -5,43 +5,43 @@
 #include "../include/arabiantorome.h"
 #include <math.h>
 
-static const char *units = "IVX";
-static const char *dozens = "XLC";
-static const char *hundreds = "CDM";
-
-uint16_t getOrder(uint64_t num) {
-    uint64_t dozen = 10;
-    int order = 1;
-    do {
-        dozen *= 10;
-        order++;
-    } while (num / dozen);
-    return order;
-}
-
-int breakOnHundreds(uint64_t num, char *charArr, int sizeChar) {
-    int numLength = getOrder(num);
-    int size = 0;
-    int *arr = NULL;
-    arr = malloc(numLength * 4);
-    if (!arr) return SEGMENTATION_FAULT;
-    do {
-        arr[size] = num % 10;
-        num /= 10;
-        size += 1;
-    } while (num != 0);
-    int j = 0;
-    for (int i = 0; i < sizeChar; i++) {
-        if (i % 4 == 3) {
-            charArr[i] = ' ';
-        } else {
-            charArr[i] = arr[j] + '0';
-            j++;
-        }
-    }
-    free(arr);
-    return sizeChar;
-}
+//static const char *units = "IVX";
+//static const char *dozens = "XLC";
+//static const char *hundreds = "CDM";
+//
+//uint16_t getOrder(uint64_t num) {
+//    uint64_t dozen = 10;
+//    int order = 1;
+//    do {
+//        dozen *= 10;
+//        order++;
+//    } while (num / dozen);
+//    return order;
+//}
+//
+//int breakOnHundreds(uint64_t num, char *charArr, int sizeChar) {
+//    int numLength = getOrder(num);
+//    int size = 0;
+//    int *arr = NULL;
+//    arr = malloc(numLength * 4);
+//    if (!arr) return SEGMENTATION_FAULT;
+//    do {
+//        arr[size] = num % 10;
+//        num /= 10;
+//        size += 1;
+//    } while (num != 0);
+//    int j = 0;
+//    for (int i = 0; i < sizeChar; i++) {
+//        if (i % 4 == 3) {
+//            charArr[i] = ' ';
+//        } else {
+//            charArr[i] = arr[j] + '0';
+//            j++;
+//        }
+//    }
+//    free(arr);
+//    return sizeChar;
+//}
 
 int checkNumber(int64_t num) {
     if (num <= 0) {
@@ -101,64 +101,64 @@ char *getRomeNumber(uint64_t decNum, char *romeNumber, size_t *size) {
     return romeNumber;
 }
 
-int addCount(char num, enum order type, char *romeString, size_t *size) {
-    char orderSting[4];
-    if (type == THOUSAND) snprintf(orderSting, sizeof(orderSting), "%s", units);
-    if (type == HUNDRED) snprintf(orderSting, sizeof(orderSting), "%s", hundreds);
-    if (type == DOZEN) snprintf(orderSting, sizeof(orderSting), "%s", dozens);
-    if (type == UNIT) snprintf(orderSting, sizeof(orderSting), "%s", units);
-    if ((num - '0') < 4) {
-        for (int i = (num - '0'); i > 0; i--) {
-            *size += 1;
-            char *checkRealloc = realloc(romeString, *size);
-            if (!checkRealloc) {
-                free(romeString);
-                return SEGMENTATION_FAULT;;
-            }
-            romeString[*size - 1] = orderSting[0];
-        }
-        return 0;
-    }
-    if ((num - '0') < 9) {
-        if ((num - '0') == 4) {
-            *size += 2;
-            char *checkRealloc = realloc(romeString, *size);
-            if (!checkRealloc) {
-                free(romeString);
-                return SEGMENTATION_FAULT;;
-            }
-            romeString[*size - 2] = orderSting[0];
-            romeString[*size - 1] = orderSting[1];
-        } else {
-            *size += 1;
-            romeString = realloc(romeString, *size);
-            char *checkRealloc = realloc(romeString, *size);
-            if (!checkRealloc) {
-                free(romeString);
-                return SEGMENTATION_FAULT;;
-            }
-            romeString[*size - 1] = orderSting[1];
-            for (int i = (num - '0'); i > 5; --i) {
-                *size += 1;
-                char *checkRealloc = realloc(romeString, *size);
-                if (!checkRealloc) {
-                    free(romeString);
-                    return SEGMENTATION_FAULT;;
-                }
-                romeString[*size - 1] = orderSting[0];
-            }
-        }
-        return 0;
-    } else {
-        *size += 2;
-        char *checkRealloc = realloc(romeString, *size);
-        if (!checkRealloc) {
-            free(romeString);
-            return SEGMENTATION_FAULT;;
-        }
-        romeString = checkRealloc;
-        romeString[*size - 2] = orderSting[0];
-        romeString[*size - 1] = orderSting[2];
-        return 0;
-    }
-}
+//int addCount(char num, enum order type, char *romeString, size_t *size) {
+//    char orderSting[4];
+//    if (type == THOUSAND) snprintf(orderSting, sizeof(orderSting), "%s", units);
+//    if (type == HUNDRED) snprintf(orderSting, sizeof(orderSting), "%s", hundreds);
+//    if (type == DOZEN) snprintf(orderSting, sizeof(orderSting), "%s", dozens);
+//    if (type == UNIT) snprintf(orderSting, sizeof(orderSting), "%s", units);
+//    if ((num - '0') < 4) {
+//        for (int i = (num - '0'); i > 0; i--) {
+//            *size += 1;
+//            char *checkRealloc = realloc(romeString, *size);
+//            if (!checkRealloc) {
+//                free(romeString);
+//                return SEGMENTATION_FAULT;;
+//            }
+//            romeString[*size - 1] = orderSting[0];
+//        }
+//        return 0;
+//    }
+//    if ((num - '0') < 9) {
+//        if ((num - '0') == 4) {
+//            *size += 2;
+//            char *checkRealloc = realloc(romeString, *size);
+//            if (!checkRealloc) {
+//                free(romeString);
+//                return SEGMENTATION_FAULT;;
+//            }
+//            romeString[*size - 2] = orderSting[0];
+//            romeString[*size - 1] = orderSting[1];
+//        } else {
+//            *size += 1;
+//            romeString = realloc(romeString, *size);
+//            char *checkRealloc = realloc(romeString, *size);
+//            if (!checkRealloc) {
+//                free(romeString);
+//                return SEGMENTATION_FAULT;;
+//            }
+//            romeString[*size - 1] = orderSting[1];
+//            for (int i = (num - '0'); i > 5; --i) {
+//                *size += 1;
+//                char *checkRealloc = realloc(romeString, *size);
+//                if (!checkRealloc) {
+//                    free(romeString);
+//                    return SEGMENTATION_FAULT;;
+//                }
+//                romeString[*size - 1] = orderSting[0];
+//            }
+//        }
+//        return 0;
+//    } else {
+//        *size += 2;
+//        char *checkRealloc = realloc(romeString, *size);
+//        if (!checkRealloc) {
+//            free(romeString);
+//            return SEGMENTATION_FAULT;;
+//        }
+//        romeString = checkRealloc;
+//        romeString[*size - 2] = orderSting[0];
+//        romeString[*size - 1] = orderSting[2];
+//        return 0;
+//    }
+//}
