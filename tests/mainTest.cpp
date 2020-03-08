ef3c1ea3408c451455d53074f96f1e1adce60abd
Dellvin <dellvin.black@gmail.com>
2020-03-08 15:35:23 +0300

    delete part of tests

--- tests/mainTest.cpp
+++ tests/mainTest.cpp
@@ -11,26 +11,26 @@ extern "C" {
 }
 
 
-TEST(test, Test1) {
-ASSERT_EQ(true, true);
-int64_t decNumber = 123456;
-size_t sizeRomeStr = 0;
-char *romeStr = NULL;
-romeStr=getRomeNumber(decNumber, romeStr, &sizeRomeStr);
-ASSERT_EQ(sizeRomeStr, 11);
-ASSERT_EQ(strcmp(romeStr, "CXXIIICDLVI"), 0);
-free(romeStr);
-}
-
-TEST(test, Test2) {
-long long num = 987;
-size_t sizeRomeStr = 0;
-char *Str = (char*)malloc(sizeRomeStr + 1);
-Str=getRomeNumber(num, Str, &sizeRomeStr);
-ASSERT_EQ(sizeRomeStr, 9);
-ASSERT_EQ(strcmp(Str, "CMLXXXVII"), 0);
-free(Str);
-}
+//TEST(test, Test1) {
+//ASSERT_EQ(true, true);
+//int64_t decNumber = 123456;
+//size_t sizeRomeStr = 0;
+//char *romeStr = NULL;
+//romeStr=getRomeNumber(decNumber, romeStr, &sizeRomeStr);
+//ASSERT_EQ(sizeRomeStr, 11);
+//ASSERT_EQ(strcmp(romeStr, "CXXIIICDLVI"), 0);
+//free(romeStr);
+//}
+//
+//TEST(test, Test2) {
+//long long num = 987;
+//size_t sizeRomeStr = 0;
+//char *Str = (char*)malloc(sizeRomeStr + 1);
+//Str=getRomeNumber(num, Str, &sizeRomeStr);
+//ASSERT_EQ(sizeRomeStr, 9);
+//ASSERT_EQ(strcmp(Str, "CMLXXXVII"), 0);
+//free(Str);
+//}
 
 TEST(test, FailTest) {
 size_t num=0;
