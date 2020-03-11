fa480675a1098ed07438d3d3c64e203888e06601
Dellvin <dellvin.black@gmail.com>
2020-03-11 14:43:51 +0300

    another try

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <lib.c>
+#include <lib.h>
 /*Условие ИЗ№1:
 Создать структуру для хранения информации об HTML-теге: его имени, признаке «открывающий/закрывающий» и атрибутах тега.
  Составить с ее использованием программу, включающую в себя функцию, принимающую на вход текстовую строку с одним тегом.
@@ -10,24 +8,17 @@
 
 
 int main() {
-
-  char ** s = (char**)calloc(1, sizeof(char*));
-  size_t * size = (size_t*)calloc(1, sizeof(size_t));
-  getline(s,size,stdin);
-  (*s)[strlen(*s)-1]='\0';
-  Tag *tag = initialize_tag(*s);
-  if (!tag) {
-    printf("ERr2");
+    int64_t decNum = 98;
+    printf("The arabian number is: %ld", decNum);
+    if (decNum <= 0) {
+        printf("Error number\nIt should be greater then zero\n");
+        return 0;
+    }
+    size_t size = 0;
+    char *answer = NULL;
+    answer = getRomeNumber(decNum, answer, &size);
+    printf("\nRome number is: ");
+    printf("%s", answer);
+    printf("\nsize: %lu\n", size);
     return 0;
-  }
-  unsigned num = amount_atributes_number(*s);
-  for (int j = 0; j < num; j++) {
-    free(tag->atributes[j]);
-  }
-  free(tag->atributes);
-  free(tag->name);
-  free(tag);
-  free(size);
-  free(*s);
-  free(s);
 }
